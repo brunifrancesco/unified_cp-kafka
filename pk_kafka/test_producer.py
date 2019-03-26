9f96e501babd00747fd989370061a963c44e236b
Francesco Bruni <bruni@planetek.it>
2019-03-26 10:12:39 +0100

    Add kafka rest producer

--- pk_kafka/test_producer.py
+++ pk_kafka/test_producer.py
@@ -1,10 +1,17 @@
-from producer import KafkaProducer
+from pk_kafka.producer import KafkaProducer, KafkaRestProducer
 
 
 def handle_json_message_data(msg):
     print(msg)
 
 
-KafkaProducer(
-    broker_address='127.0.1.1',
-).publish_message('myTopic', {'key': 2})
+# KafkaProducer(
+#     broker_address='127.0.1.1',
+# ).publish_message('myTopic', {'key': 2})
+
+
+result = KafkaRestProducer(
+    broker_address='http://spinua.rheticus.eu:5000/kafka-rest-proxy',
+    credentials=('dockermanager', 'CTGsSH4e5x')
+).publish_message('removeme', {'vediamo':'sefunziona'})
+print(result.text)
